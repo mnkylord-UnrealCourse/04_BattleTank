25afab9e1ba6203d68ee72c0a90aae28d1a77bbe
Tony Fan <digitalcyclops@gmail.com>
2018-11-02 23:25:51 -0500

    added GetControlledTank() method and override BeginPlay() method in TankPlayerController class added debug log statements

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -8,3 +8,17 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to get controlled tank!!"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController::BeginPlay() @ (%s)"), *GetControlledTank()->GetActorLocation().ToString());
+	}
+}
