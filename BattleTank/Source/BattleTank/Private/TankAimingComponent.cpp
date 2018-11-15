433ab21bff63403254663b6103aea38c7f3bea28
Tony Fan <digitalcyclops@gmail.com>
2018-11-14 22:11:47 -0600

    calculated delta rotator (for barrel's up/down rotation) removed BeginPlay() and Tick() in TankAimingComponent

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -13,23 +13,6 @@ UTankAimingComponent::UTankAimingComponent()
 	// ...
 }
 
-
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-}
-
 void UTankAimingComponent::AimAt(FVector HitLocation, float FiringSpeed)
 {
 	if (!Barrel) { return;  }
@@ -42,17 +25,23 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float FiringSpeed)
 		StartLocation,
 		HitLocation,
 		FiringSpeed,
-		false,
-		0.0f,
-		0,
 		ESuggestProjVelocityTraceOption::DoNotTrace
 	);
 
 	if (success)
 	{
 		FVector AimDirection = OutFiringVelocity.GetSafeNormal();
-		UE_LOG(LogTemp, Warning, TEXT("%s aiming toward (%s) at %f cm/s"), *GetOwner()->GetName(), *AimDirection.ToString(), FiringSpeed);
+		MoveBarrelToward(AimDirection);
+		//UE_LOG(LogTemp, Warning, TEXT("%s aiming toward (%s) at %f cm/s"), *GetOwner()->GetName(), *AimDirection.ToString(), FiringSpeed);
 	}
+}
+
+void UTankAimingComponent::MoveBarrelToward(FVector AimDirection)
+{
+	FRotator BarrelRotator = Barrel->GetForwardVector().Rotation();
+	FRotator AimRotator = AimDirection.Rotation();
+	FRotator DeltaRotator = AimRotator - BarrelRotator;
+	UE_LOG(LogTemp, Warning, TEXT("%s rotate barrel (%s)"), *GetOwner()->GetName(), *DeltaRotator.ToString());
 }
 
 void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
